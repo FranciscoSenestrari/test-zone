5f57ee2c038b3616e12512efc67474c31bfff191
FranciscoSenestrari <fran.senes@gmail.com>
2022-07-22 09:11:29 -0300

    add image item

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Icon from './components/svg/cop.svg'
-
+import Item from  './components/Item'
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Holas!</Text>
+      <Item />
       <StatusBar style="auto" />
     </View>
   );
--- components/Item.js
+++ components/Item.js
@@ -1,16 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 import React from "react";
 import Icon from "./svg/user.svg";
 
 const Item = () => {
   return (
-    <View>
-          <Text>Item</Text>
-          <Text>Item</Text>
-
-
+    <View style={styles.container}>
+      <Image source={require('/components/svg/feet.png')} />
     </View>
   );
 };
 
 export default Item;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 150,
+    width: 120,
+    backgroundColor: "#f4f4f4",
+    borderRadius: 20,
+    shadowColor: "#c1c1c1",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 10,
+  },
+});
